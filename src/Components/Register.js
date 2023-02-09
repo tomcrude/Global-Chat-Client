0c80e15ba9ccf908d7675e4d3fe00c902c39d7e7
angelo <angesmor12@hotmail.com>
2023-02-09 18:54:19 -0300

    ready

---
+++ src/Components/Register.js
@@ -0,0 +1,69 @@
+import React, {  useEffect, useState } from "react";
+import {useNavigate} from "react-router-dom"
+
+export default function Register() {
+    
+
+    const navigate = useNavigate()
+
+    const [active, setactive] = useState(true)  
+
+    const [repit, setrepit] = useState([])
+
+    const [name, setname] = useState("")
+
+    const [x, setx] = useState(0)
+
+
+    useEffect(()=>{
+      
+      fetch("https://global-chat.onrender.com/api")
+      .then(res => {return res.json()})
+      .then(respo => setrepit(respo))
+      
+  
+
+    })
+
+    window.localStorage.setItem("user", "")
+
+    return (
+    <div className="container text-center mt-5 register position-absolute t-t">
+
+    
+
+      <div className="row">
+    <h1 className="col-12 mt-5 title">Global Chat</h1>
+    <h2 className="col-12 mt-5">Insert your name</h2>
+    <form>
+        <input placeholder="Max 10 characters" className="col-12 mt-3 input text-center mb-3" required onChange={(e)=>{setname(e.target.value)}} type="text" />
+        <input className="col-12 mt-4 buton" type="submit" onClick={async (e)=>{
+          e.preventDefault()
+          
+        
+          if (!name){document.querySelector(".error").innerHTML = "Enter a name";setactive(false)}
+          else if (name.length > 10){document.querySelector(".error").innerHTML = "The name must not contain more than 10 characters"
+           ;setactive(false)}
+          else{
+          repit.map((stat)=>{
+            if (name === stat.user){setx(2); window.location.reload();}
+            if (stat.id === repit.length && x === 0) {window.localStorage.setItem("user", name);navigate('/chat')}
+          })}
+          
+          setx(0)
+          
+          }} />
+    </form>
+    <h3 className={active ? "col-12 mt-5 error inactive" : "col-12 mt-5 error"}>Please choose a correct name</h3>
+
+    <footer className="col-12 text-center mt-5" >
+          <p className="text-muted text-decoration-underline">Angelo Smorlesi</p>
+    </footer>
+
+    </div>
+    </div>
+  )
+
+}
+
+
